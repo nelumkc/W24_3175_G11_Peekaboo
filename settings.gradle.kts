ff117f5c0d9fd747aa83b60b1fdecad834537adc
nelukc <nelum_19@yahoo.com>
2024-02-16 10:41:28 -0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "W24_3175_G11_Peekaboo"
+include(":app")
+ 
