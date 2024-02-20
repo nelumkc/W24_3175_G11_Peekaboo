111b07abf127a882c5b12189b92124647c0f5d4f
nelukc <nelum_19@yahoo.com>
2024-02-19 22:37:48 -0800

    New Message Fragment added

---
+++ app/src/main/java/com/example/w24_3175_g11_peekaboo/NewMessageActivity.java
@@ -0,0 +1,14 @@
+package com.example.w24_3175_g11_peekaboo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+    }
+}
