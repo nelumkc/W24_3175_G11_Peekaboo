56e3e1d43a81d06eb9a975bbfad27727f491f873
nethmidsdouglas <majuwanegamagen@student.douglascollege.ca>
2024-03-20 16:09:46 -0700

    Adding Google APIkey and Splash Activity

---
+++ app/src/main/java/com/example/w24_3175_g11_peekaboo/activities/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.w24_3175_g11_peekaboo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.w24_3175_g11_peekaboo.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        // Using TimerTask and Timer
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                // Start the MainActivity after 3000 milliseconds (3 seconds)
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, 3000); // Schedule the task to run after 3000 milliseconds (3 seconds)
+    }
+}
