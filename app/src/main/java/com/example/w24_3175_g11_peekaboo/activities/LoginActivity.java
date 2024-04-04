ec0c3e1b9836bc70f48676f08dcef57470ec50d5
nelumkc <rangebandaramuc@student.douglascollege.ca>
2024-04-04 16:53:32 -0700

    passing the email to main activity

--- app/src/main/java/com/example/w24_3175_g11_peekaboo/activities/LoginActivity.java
+++ app/src/main/java/com/example/w24_3175_g11_peekaboo/activities/LoginActivity.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -119,6 +120,7 @@ public class LoginActivity extends AppCompatActivity {
         googleSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Test", Toast.LENGTH_SHORT).show();
                 Intent intent = mGoogleSignInClient.getSignInIntent();
                 googleSignInLauncher.launch(intent);
             }
@@ -197,6 +199,7 @@ public class LoginActivity extends AppCompatActivity {
             firebaseAuthWithGoogle(account.getIdToken());
         } catch (ApiException e) {
             e.printStackTrace();
+            Log.w("PEEKABOO", "signInResult:failed code=" + e.getStatusCode());
         }
     }
 
@@ -210,6 +213,7 @@ public class LoginActivity extends AppCompatActivity {
                             FirebaseUser user = firebaseAuth.getCurrentUser();
                             if (user != null) {
                                 String userEmail = user.getEmail();
+                                //Toast.makeText(LoginActivity.this, "userEmail" + userEmail, Toast.LENGTH_SHORT).show();
                                 checkUserRegistration(userEmail);
                             } else {
                                 Toast.makeText(LoginActivity.this, "Error during sign in. Please try again.", Toast.LENGTH_SHORT).show();
@@ -234,7 +238,10 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(LoginActivity.this, "Your email is not registered in our system. Please contact support.", Toast.LENGTH_LONG).show();
                             firebaseAuth.signOut();
                         }else{
-                            navigateToMainActivity();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.putExtra("userEmail", userEmail);
+                            startActivity(intent);
+                            finish();
                         }
                     }
                 });
